7be156b5c6bd7dabb8545e01ddbcc576124b31da
thanmaigouru <gouruthanmai@gmail.com>
2023-06-18 18:41:08 -0500

    Adding odd even files

---
+++ Learning/src/OddEven/NumberCheck.java
@@ -0,0 +1,17 @@
+package OddEven;
+
+public class NumberCheck {
+
+    void checkNumber(int a){
+
+        if(a % 2 == 0) {
+
+            System.out.println("Given number is Even");
+        }
+            else {
+                System.out.println("Given number is Odd");
+
+            }
+
+    }
+}
---
+++ Learning/src/OddEven/OddEvenMain.java
@@ -0,0 +1,11 @@
+package OddEven;// Write a program to check if a given number is odd or even.
+
+public class OddEvenMain {
+    public static void main(String[] args) {
+
+        NumberCheck numbercheck = new NumberCheck();
+
+        numbercheck.checkNumber(5);
+
+        }
+    }
